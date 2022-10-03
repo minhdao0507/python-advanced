87d2f42e569aeaa775d030ef9196fc9fee7f4d32
minhdao0507 <you@example.comdphm57@gmail.com>
2022-10-03 15:21:27 +0700

    update lesson 3

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "user_auth",
+    "myapp",
     "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -13,3 +13,34 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+# class MyPerson(Person):
+# 	age = xxx
+#     class Meta:
+#         proxy = True
+ 
+#     def do_something(self):
+#         pass
+    person_id = models.AutoField(primary_key=True)
+    person_name = models.CharField(max_length=100)
+    person_age = models.PositiveIntegerField()
+
+class Manufacturer(models.Models):
+	manufacturer = models.CharField(max_length=50)
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer,on_delete=models.CASCADE)
+    car_brand = models.CharField(max_length=50)
