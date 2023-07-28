7ff3cd1b9282fd0068de640aeb0279720755946c
minhdao0507 <you@example.comdphm57@gmail.com>
2023-07-28 22:29:53 +0700

    Update old

--- final_project/finalProject/myProject/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import myProject
-
-class proForm(forms.ModelForm):
-    class Meta:
-        model = myProject
-        fields = ['id', 'name', 'about_pro', 'tech_used', 'source', 'status']
--- final_project/finalProject/weather/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import WeatherAPI
-urlpatterns = [
-    path('weather', WeatherAPI.as_view()),
-]
